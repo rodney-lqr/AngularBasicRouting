1d2855c3840e1be4df636e76a63184633c52c5a6
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-09-17 11:31:49 +0800

    Creating a components that consists of four items and finished routing for each files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'homepage-component',
+        component: HomepageComponent
+    },
+    {
+        path: 'landingpage-component',
+        component: LandingpageComponent
+    },
+    {
+        path: 'login-component',
+        component: LoginComponent
+    },
+    {
+        path: 'signup-component',
+        component: SignupComponent
+    },
+];
